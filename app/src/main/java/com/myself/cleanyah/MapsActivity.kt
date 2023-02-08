896f6b20169b65e03ad05a0e91466ff3ffde3971
g6035210 <g6035210>
2023-02-08 13:41:05 +0900

    add image

--- app/src/main/java/com/myself/cleanyah/MapsActivity.kt
+++ app/src/main/java/com/myself/cleanyah/MapsActivity.kt
@@ -1,10 +1,12 @@
 package com.myself.cleanyah
 
 import android.Manifest
+import android.R.drawable.*
 import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.os.Handler
@@ -20,9 +22,12 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.myself.cleanyah.databinding.ActivityMapsBinding
+import java.util.*
 
 
 @Suppress("DEPRECATION")
@@ -31,6 +36,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private var mDialog: AlertDialog? = null
+    private var mTimeCount = 0
+    private val mLatVal = arrayOf(0.0, 0.0)
+    private var mIsCountIncrease = true
+    private val mMoveCatIconTimer = Timer()
+
     companion object {
         const val CAMERA_REQUEST_CODE = 1
         const val CAMERA_PERMISSION_REQUEST_CODE = 2
@@ -48,12 +58,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         // タグのIDを取得
-        val tagId : ByteArray =intent.getByteArrayExtra(NfcAdapter.EXTRA_ID) ?: return
+        val tagId: ByteArray = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID) ?: return
         val list = ArrayList<String>()
-        for(byte in tagId) {
+        for (byte in tagId) {
             list.add(String.format("%02X", byte.toInt() and 0xFF))
         }
-        Log.d("NFC Id",list.joinToString(":"))
+        Log.d("NFC Id", list.joinToString(":"))
     }
 
     override fun onResume() {
@@ -68,6 +78,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
             } ?: Toast.makeText(this, "カメラを扱うアプリがありません", Toast.LENGTH_LONG).show()
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mMoveCatIconTimer.cancel()
     }
 
     private fun showMessage(text: String) {
@@ -133,18 +148,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
 
     private fun grantCameraPermission() =
-        ActivityCompat.requestPermissions(this,
+        ActivityCompat.requestPermissions(
+            this,
             arrayOf(Manifest.permission.CAMERA),
-            CAMERA_PERMISSION_REQUEST_CODE)
+            CAMERA_PERMISSION_REQUEST_CODE
+        )
 
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<out String>,
-                                            grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() &&
-                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
                 takePicture()
             }
         }
@@ -165,7 +185,50 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Add a marker in Sydney and move the camera
         //val sydney = LatLng(-34.0, 151.0)
         val kawasaki = LatLng(35.5319793421874, 139.69685746995052)
-        mMap.addMarker(MarkerOptions().position(kawasaki).title("Marker!"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kawasaki, 15F))
+        val marker = MarkerOptions().position(kawasaki).title("Marker!")
+        mMap.addMarker(marker)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kawasaki, 16.5F))
+
+        val MIN_LAT_LNG = arrayOf(35.531685, 139.694082)
+        //val MAX_LAT_LNG = arrayOf(35.531711, 139.695253)
+
+        val catMarker = MarkerOptions().position(LatLng(MIN_LAT_LNG[0], MIN_LAT_LNG[1])).title("Cat")
+        catMarker.icon(getMakerIcon(R.drawable.cat_image))
+        mMap.addMarker(catMarker)
+        mTimeCount = 0
+        mLatVal[0] = MIN_LAT_LNG[0]
+        mLatVal[1] = MIN_LAT_LNG[1]
+
+        mMoveCatIconTimer.scheduleAtFixedRate(
+            object : TimerTask() {
+                override fun run() {
+                    if (mIsCountIncrease) {
+                        mTimeCount += 1
+                        if (mTimeCount % 2 == 0)
+                            mLatVal[0] += 0.000001
+                        mLatVal[1] += 0.00002
+                        if (mTimeCount == 60)
+                            mIsCountIncrease = false
+                    } else {
+                        mTimeCount -= 1
+                        if (mTimeCount % 2 == 0)
+                            mLatVal[0] -= 0.000001
+                        mLatVal[1] -= 0.00002
+                        if (mTimeCount == 0)
+                            mIsCountIncrease = true
+                    }
+                    catMarker.position(LatLng(mLatVal[0], mLatVal[1]))
+                    //Log.i(LOG_TAG, "Task called.")
+                }
+            }, 10, 1000
+        )
+    }
+
+    private fun getMakerIcon(id: Int): BitmapDescriptor {
+        val width = 170
+        val height = 85
+        val b = BitmapFactory.decodeResource(resources, id)
+        val smallMarker = Bitmap.createScaledBitmap(b, width, height, false)
+        return BitmapDescriptorFactory.fromBitmap(smallMarker)
     }
 }
