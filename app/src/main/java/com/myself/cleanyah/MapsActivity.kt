a2e9871e01d051bbbaba9528612a7c0baef86509
fluma <flum.ar.ik@gmail.com>
2023-02-10 08:08:26 +0900

    NFC complete

--- app/src/main/java/com/myself/cleanyah/MapsActivity.kt
+++ app/src/main/java/com/myself/cleanyah/MapsActivity.kt
@@ -1,7 +1,6 @@
 package com.myself.cleanyah
 
 import android.Manifest
-import android.R.drawable
 import android.app.AlertDialog
 import android.app.PendingIntent
 import android.content.ContentValues
@@ -31,15 +30,13 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import com.myself.cleanyah.card_emu.HostCardEmulatorService
 import com.myself.cleanyah.databinding.ActivityMapsBinding
-import java.security.AccessController.getContext
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -52,11 +49,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private var mTimeCount = 0
     private val mLatVal = arrayOf(0.0, 0.0)
     private var mIsCountIncrease = true
-    private val mMoveCatIconTimer = Timer()
+    private var mIsTimerStart = false
+    private var mIsMapStart = false
+    private var mMoveCatIconThread = Thread()
     private lateinit var mAdapter: NfcAdapter
     private lateinit var mPendingIntent: PendingIntent
     private lateinit var mFilters: Array<IntentFilter>
     private lateinit var mTechLists: Array<Array<String>>
+    private var mMakerKawasaki: Marker? = null
+    private var mMakerCat: Marker? = null
+    private var mMakerTrash: Marker? = null
+    private val mLock = ReentrantLock()
+    private val mTrashMap: MutableMap<Marker, ImageView> = mutableMapOf()
+    private var mIsNfcAuthentication = false
     companion object {
         const val PERMISSION_REQUEST_CODE = 1
         const val NFC_ACTION = NfcAdapter.ACTION_TAG_DISCOVERED
@@ -89,12 +94,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mAdapter.enableReaderMode(
             this,
-            {
-                Handler(Looper.getMainLooper()).postDelayed({
-                    showPointMessage("清掃ありがとうニャ！！", "ゴミを片付けました　　+10pt!")
-                    reloadMaker(true, false)
-                }, 100)
-            },
+            { mIsNfcAuthentication = true },
             NfcAdapter.FLAG_READER_NFC_F,
             null
         )
@@ -107,6 +107,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onResume()
         setCameraButton()
         mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters, mTechLists)
+        if (!mIsTimerStart) {
+            mIsTimerStart = true
+            startMoveCatIcon()
+        }
     }
     override fun onPause() {
         super.onPause()
@@ -116,7 +120,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
     override fun onStop() {
         super.onStop()
-        mMoveCatIconTimer.cancel()
+        if (mIsTimerStart) {
+            mIsTimerStart = false
+            mMoveCatIconThread.interrupt()
+        }
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -136,27 +143,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun showMessage(text: String) {
+    private fun showMessage(title: String?, text: String) {
         val builder = AlertDialog.Builder(this)
-        builder.setMessage(text)
-        mDialog = builder.create()
-        mDialog.let {
-            Handler(Looper.getMainLooper()).postDelayed({
-                it!!.dismiss()
-            }, 3000)
-            it!!.show()
+        if (title != null) {
+            builder.setTitle(title).setIcon(R.drawable.cat_image2)
         }
-    }
-    private fun showPointMessage(title: String, text: String) {
-        val builder = AlertDialog.Builder(this)
         builder.setMessage(text)
         mDialog = builder.create()
         mDialog?.let {
-            val imageView = ImageView(this)
-            imageView.setImageResource(R.drawable.cat_image2)
-            it.setTitle(title)
-            it.setView(imageView)
-            it.setMessage(text)
             Handler(Looper.getMainLooper()).postDelayed({
                 it.dismiss()
             }, 3000)
@@ -176,8 +170,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     Handler(Looper.getMainLooper()).postDelayed({
                         mDialog!!.dismiss()
                         window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
-                        showPointMessage("報告ありがとうニャ！！", "画像を報告しました　　+5pt!")
-                        reloadMaker(true, true)
+                        showMessage("報告ありがとうニャ！！", "ゴミを報告しました　　+5pt!")
+                        setTrashMaker()?.let { it1 -> mTrashMap[it1] = imageView }
                     }, 7000)
                     mDialog!!.show()
                     window.setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
@@ -185,7 +179,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
             }
             .setNegativeButton("いいえ") { _, _ ->
-                showMessage("画像の送信をキャンセルしました")
+                showMessage(null, "画像の送信をキャンセルしました")
             }
             .setView(imageView)
             .show()
@@ -210,10 +204,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         if (requestCode == PERMISSION_REQUEST_CODE)
             if (grantResults.isNotEmpty())
                 if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    if (PackageManager.PERMISSION_GRANTED == checkSelfPermission(Manifest.permission.CAMERA))
-                        if (PackageManager.PERMISSION_GRANTED == checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE))
-                            if (PackageManager.PERMISSION_GRANTED == checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE))
-                                takePicture()
+                    checkPermissions()
     }
     private fun checkPermission(permission: String): Boolean {
         var ret = true
@@ -224,23 +215,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
         return ret
     }
+    private fun checkPermissions() {
+        var isTakePicture = true
+        if (!checkPermission(Manifest.permission.CAMERA)) {
+            isTakePicture = false
+        }
+        if (!checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            isTakePicture = false
+        }
+        if (!checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            isTakePicture = false
+        }
+        if (isTakePicture) {
+            takePicture()
+        }
+    }
     private fun setCameraButton(){
         binding.launchButton.setOnClickListener {
             // カメラ機能を実装したアプリが存在するかチェック
             Intent(MediaStore.ACTION_IMAGE_CAPTURE).resolveActivity(packageManager)?.let {
-                var isTakePicture = true
-                if (!checkPermission(Manifest.permission.CAMERA)) {
-                    isTakePicture = false
-                }
-                if (!checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                    isTakePicture = false
-                }
-                if (!checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                    isTakePicture = false
-                }
-                if (isTakePicture) {
-                    takePicture()
-                }
+                checkPermissions()
             } ?: Toast.makeText(this, "カメラを扱うアプリがありません", Toast.LENGTH_LONG).show()
         }
 
@@ -253,84 +247,127 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val smallMarker = Bitmap.createScaledBitmap(b, width, height, false)
         return BitmapDescriptorFactory.fromBitmap(smallMarker)
     }
-    private fun setMaker(latitude: Double, longitude: Double, title: String, id: Int?): MarkerOptions {
+    private fun setMainMaker(): Marker? {
         val options = MarkerOptions()
-        options.position(LatLng(latitude, longitude))
-        options.title(title)
-        if (id != null)
-            options.icon(getMakerIcon(id))
-        return options
+        options.position(LatLng(35.5319793421874, 139.69685746995052))
+        options.title("Marker!")
+        options.draggable(true)
+        return mMap.addMarker(options)
     }
-
-    private lateinit var kawasakiMaker: MarkerOptions
-    private lateinit var catMaker: MarkerOptions
-    private lateinit var trashMaker: MarkerOptions
-    private fun moveCatMaker(double1: Double, double2: Double){
-        catMaker.position(LatLng(double1, double2))
+    private fun setCatMaker(): Marker? {
+        val options = MarkerOptions()
+        options.position(LatLng(35.531685, 139.694082))
+        options.title("Cat!")
+        options.icon(getMakerIcon(R.drawable.spot_cat))
+        return mMap.addMarker(options)
     }
-
-
-    private fun reloadMaker(isTrashPointChange: Boolean, isTrashPoint: Boolean){
-        /*/mMap.clear()
-        mMap.addMarker(kawasakiMaker)
-        mMap.addMarker(catMaker)
-        if (isTrashPointChange)
-            trashMaker.visible(isTrashPoint)
-        mMap.addMarker(trashMaker)
-
-         */
+    private fun setTrashMaker(): Marker? {
+        val options = MarkerOptions()
+        options.position(LatLng(35.531711, 139.6952532))
+        options.title("Trash!")
+        options.icon(getMakerIcon(R.drawable.spot_gomi))
+        val marker = mMap.addMarker(options)
+        marker?.let { it.position = mMakerKawasaki?.position!! }
+        return marker
     }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        kawasakiMaker = setMaker(35.5319793421874, 139.69685746995052, "Marker!", null)
-        catMaker = setMaker(35.531685, 139.694082, "Cat!", R.drawable.spot_cat)
-        trashMaker = setMaker(35.531711, 139.6952532, "Trash!", R.drawable.spot_gomi)
-        // Add a marker in Sydney and move the camera
-        //val sydney = LatLng(-34.0, 151.0)
-        //val kawasaki = LatLng(35.5319793421874, 139.69685746995052)
-        val marker = catMaker
-        //mMap.addMarker(marker)
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(35.5319793421874, 139.69685746995052), 16.5F))
-        mMap.addMarker(kawasakiMaker)
-        mMap.addMarker(catMaker)
-        mMap.addMarker(trashMaker)
-
-        val MIN_LAT_LNG = arrayOf(35.531685, 139.694082)
-        //val MAX_LAT_LNG = arrayOf(35.531711, 139.695253)
-
-        //val catMarker = MarkerOptions().position(LatLng(MIN_LAT_LNG[0], MIN_LAT_LNG[1])).title("Cat")
-        //catMarker.icon(getMakerIcon(R.drawable.spot_cat))
-        //mMap.addMarker(catMarker)
-        mTimeCount = 0
-        mLatVal[0] = MIN_LAT_LNG[0]
-        mLatVal[1] = MIN_LAT_LNG[1]
-
-        mMoveCatIconTimer.scheduleAtFixedRate(
-            object : TimerTask() {
-                override fun run() {
-                    if (mIsCountIncrease) {
-                        mTimeCount += 1
-                        if (mTimeCount % 2 == 0)
-                            mLatVal[0] += 0.000001
-                        mLatVal[1] += 0.00002
-                        if (mTimeCount == 60)
-                            mIsCountIncrease = false
-                    } else {
-                        mTimeCount -= 1
-                        if (mTimeCount % 2 == 0)
-                            mLatVal[0] -= 0.000001
-                        mLatVal[1] -= 0.00002
-                        if (mTimeCount == 0)
-                            mIsCountIncrease = true
+    private fun startMoveCatIcon() {
+        mMoveCatIconThread = Thread {
+            this.runOnUiThread {
+                while (true) {
+                    Thread.sleep(1000)
+                    if (mIsMapStart) {
+                        if (mIsCountIncrease) {
+                            mTimeCount += 1
+                            if (mTimeCount % 2 == 0)
+                                mLatVal[0] += 0.000001
+                            mLatVal[1] += 0.00002
+                            if (mTimeCount == 60)
+                                mIsCountIncrease = false
+                        } else {
+                            mTimeCount -= 1
+                            if (mTimeCount % 2 == 0)
+                                mLatVal[0] -= 0.000001
+                            mLatVal[1] -= 0.00002
+                            if (mTimeCount == 0)
+                                mIsCountIncrease = true
+                        }
+                        mMakerCat?.position = LatLng(mLatVal[0], mLatVal[1])
+                        //moveCatMaker(mLatVal[0], mLatVal[1])
+                        //reloadMaker(isTrashPointChange = false, isTrashPoint = false)
                     }
-                    moveCatMaker(mLatVal[0], mLatVal[1])
-                    reloadMaker(false, false)
-                    //Log.i(LOG_TAG, "Task called.")
+                    Log.i("MainActivity", "Timer Task called.")
                 }
-            }, 10, 1000
-        )
+            }
+        }
+        //mMoveCatIconThread.start()
+    }
+    private fun removeTrashMaker(marker: Marker){
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        //val MIN_LAT_LNG = arrayOf(35.531685, 139.694082)
+        //val MAX_LAT_LNG = arrayOf(35.531711, 139.695253)
+        mMap = googleMap
+        mMakerKawasaki = setMainMaker()
+        mMakerCat = setCatMaker()
+        //mMakerTrash = setTrashMaker()
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(35.5319793421874, 139.69685746995052), 16.5F))
+        mMap.setOnMarkerDragListener(object : GoogleMap.OnMarkerDragListener {
+            override fun onMarkerDragStart(marker: Marker) {
+            }
+            override fun onMarkerDragEnd(marker: Marker) {
+                if (marker == mMakerKawasaki) {
+                    mMakerKawasaki?.let {
+                        it.position = marker.position
+                    }
+                }
+            }
+            override fun onMarkerDrag(marker: Marker) {
+            }
+        })
+        mMap.setOnMarkerClickListener { p0 ->
+            if (p0 != mMakerKawasaki && p0 != mMakerCat) {
+                val imageView = mTrashMap[p0]
+                imageView?.let {
+                    val builder = AlertDialog.Builder(this)
+                    builder.setTitle("ゴミを片付けましたか")
+                    builder.setPositiveButton("はい") { _, _ ->
+                        mIsNfcAuthentication = false
+                        val builder1 = AlertDialog.Builder(this)
+                        builder1.setMessage("くりーにゃーにスマホをタッチしてください。")
+                        //mDialog.let { it!!.dismiss() }
+                        //mDialog = builder1.create()
+                        //mDialog.let { it!!.show() }
+                        val dialog1 = builder1.create()
+                        dialog1.show()
+
+                        var count = 0
+                        while (!mIsNfcAuthentication) {
+                            Thread.sleep(1)
+                            count += 1
+                            if (count > 5000) break
+                        }
+                        dialog1.dismiss()
+                        if (mIsNfcAuthentication) {
+                            showMessage("清掃ありがとうニャ！！", "ゴミを片付けました　　+10pt!")
+                            mTrashMap.remove(p0)
+                            p0.remove()
+                        }
+                    }
+                    builder.setNegativeButton("いいえ") {_, _ ->}
+                    //builder.setView(imageView)
+                    //mDialog.let { it!!.dismiss() }
+                    //mDialog = builder.create()
+                    //mDialog.let { it!!.show() }
+                    val dialog = builder.create()
+                    dialog.show()
+                }
+            }
+            false
+        }
+        mTimeCount = 0
+        mLatVal[0] = 35.531685
+        mLatVal[1] = 139.694082
+        mIsMapStart = true
     }
 
 }
